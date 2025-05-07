c4e4c4d10be19220d6d1ecb8c78c01860f152a2d
Alumnos <alumnos@h50-006.upc.edu.pe>
2025-05-07 11:44:07 -0500

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
+import {MatAnchor} from '@angular/material/button';
+import {LanguageSwitcherComponent} from './public/components/language-switcher/language-switcher.component';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarRow, MatToolbar, MatAnchor, RouterLinkActive, RouterLink, LanguageSwitcherComponent, TranslatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Learning-Center';
+
+options= [
+  {link:'/home',label:'home'},
+  {link:'/about',label:'about'},
+  {link:'/learning/courses',label:'courses'},
+]
+
+
+
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './public/pages/home/home.component';
+const AboutComponent = () => import('./public/pages/about/about.component'). then(m => m.AboutComponent);
+const PageNotFoundComponent = () => import('./public/pages/page-not-found/page-not-found.component'). then(m => m.PageNotFoundComponent);
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', loadComponent: AboutComponent },
+  { path: '', redirectTo:'/home',pathMatch:'full'},
+  { path: '**', loadComponent: PageNotFoundComponent}
+];
