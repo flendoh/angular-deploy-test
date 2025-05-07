1b60cff3bb09dd63a720ca42eb1895da032cdeb4
Alumnos <alumnos@h50-006.upc.edu.pe>
2025-05-07 12:05:55 -0500

    Auto-generated commit

---
+++ chunk-GNNMOE3M.js
@@ -0,0 +1,1 @@
+import{Ea as o,Wa as t,Xa as a,hb as p}from"./chunk-BJEMHX5K.js";var i=class n{static \u0275fac=function(e){return new(e||n)};static \u0275cmp=o({type:n,selectors:[["app-page-not-found"]],decls:2,vars:0,template:function(e,m){e&1&&(t(0,"p"),p(1,"page-not-found works!"),a())},encapsulation:2})};export{i as PageNotFoundComponent};
---
+++ chunk-ZEF4B3TR.js
@@ -0,0 +1,1 @@
+import{Ea as o,Wa as n,Xa as a,hb as p}from"./chunk-BJEMHX5K.js";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-about"]],decls:2,vars:0,template:function(e,m){e&1&&(n(0,"p"),p(1,"about works!"),a())},encapsulation:2})};export{i as AboutComponent};
